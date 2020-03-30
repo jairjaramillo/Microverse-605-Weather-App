55b8cb504938a229f62628e3f88a23537566292d
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-03-30 09:00:53 -0600

    Starting navbar

--- src/helper/create.js
+++ src/helper/create.js
@@ -1,9 +1,14 @@
-const Create = (appendTo, className = '', idName = '', elementType = 'div') => {
-  const element = document.createElement(elementType);
-  if (className !== '') element.className = className;
-  if (idName !== '') element.id = idName;
-  if (appendTo != null) appendTo.appendChild(element);
-  return element;
-};
+class Create {
+  /**
+   * @param {{ appendChild: (arg0: HTMLElement) => void; }} appendTo
+   */
+  constructor(appendTo, className = '', idName = '', elementType = 'div') {
+    const element = document.createElement(elementType);
+    if (className !== '') element.className = className;
+    if (idName !== '') element.id = idName;
+    if (appendTo != null) appendTo.appendChild(element);
+    return element;
+  }
+}
 
 export default Create;
