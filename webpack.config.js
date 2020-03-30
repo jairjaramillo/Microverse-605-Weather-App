44964c27c4641764963cea1b68da0ab2875ea78a
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-03-29 19:34:15 -0600

    Add webpack dependencies

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: 'src/index.js',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: '[name].[chunkhash:8].js',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
