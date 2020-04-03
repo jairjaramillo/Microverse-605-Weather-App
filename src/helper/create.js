c7ca63d42d2737871ec9c43375c612cd8ed51e0c
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-03 11:13:11 -0600

    Improve documentation

--- src/helper/create.js
+++ src/helper/create.js
@@ -1,11 +1,11 @@
 /* eslint-disable max-len */
 
 /**
- * Creates an HTML element and appends it to another element.
+ * Creates an HTML element and appends it to another HTML element.
  * @param {HTMLElement} appendTo HTML element where the create function witll append to. Use NULL to avoid this.
  * @param {String} className (Optional) The class or classes the element will have.
- * @param {String} idName (Optional) The Id tag of the element.
- * @param {String} elementType (Optional) The type of the element it will be created, is <div> by default.
+ * @param {String} idName (Optional) The ID tag of the element.
+ * @param {String} elementType (Optional) The type of the element it will be created. <div> by default.
  */
 export default function create(appendTo, className = '', idName = '', elementType = 'div') {
   const element = document.createElement(elementType);
