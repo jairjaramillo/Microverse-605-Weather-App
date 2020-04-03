5a4d915292ad9c7d8e34d291788fa947d590034e
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-03 12:02:59 -0600

    Remove comment

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -5,7 +5,7 @@ export default async function fetchWeather(query, unit = 1) {
     const fetchString = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit === 1 ? 'metric' : 'imperial'}&APPID=${keyWeather()}`;
     const fetchData = await fetch(fetchString, { mode: 'cors' });
     const data = await fetchData.json();
-    // console.log(data);
+
     return [data, unit];
   } catch (error) {
     // eslint-disable-next-line no-console
