42e0da09b3e6f9c59c4aa0d82728d76dac0d9fd8
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-03 11:36:00 -0600

    Add license to the helpers

--- src/helper/create.js
+++ src/helper/create.js
@@ -1,3 +1,27 @@
+/*! *****************************************************************************
+MIT License
+
+Copyright (c) 2020 Jair Jaramillo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************* */
+
 /* eslint-disable max-len */
 
 /**
@@ -6,7 +30,7 @@
  * @param {String} className (Optional) The class or classes the element will have.
  * @param {String} idName (Optional) The ID tag of the element.
  * @param {String} elementType (Optional) The type of the element it will be created. <div> by default.
- */
+ #endregion */
 export default function create(appendTo, className = '', idName = '', elementType = 'div') {
   const element = document.createElement(elementType);
   if (className !== '') element.className = className;
