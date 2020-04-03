08c19a7a6ca25bb7f54f04b0684d58fc035e5e47
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-03 08:58:43 -0600

    Change url fetch to HTTPS

--- src/fetchNews.js
+++ src/fetchNews.js
@@ -2,7 +2,7 @@ import newsKey from './helper/newsKey';
 
 export default async function fetchNews(query) {
   try {
-    const fetchString = `http://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=${newsKey()}`;
+    const fetchString = `https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=${newsKey()}`;
     const fetchData = await fetch(fetchString, { mode: 'cors' });
     const data = await fetchData.json();
 
--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -2,7 +2,7 @@ import weatherKey from './helper/weatherKey';
 
 export default async function fetchWeather(query, unit = 1) {
   try {
-    const fetchString = `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit === 1 ? 'metric' : 'imperial'}&APPID=${weatherKey()}`;
+    const fetchString = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${unit === 1 ? 'metric' : 'imperial'}&APPID=${weatherKey()}`;
     const fetchData = await fetch(fetchString, { mode: 'cors' });
     const data = await fetchData.json();
     // console.log(data);
